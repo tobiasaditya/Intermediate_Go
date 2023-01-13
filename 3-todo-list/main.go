26d6b7d9d9f92243448dc93a87cf6af07b57fdb3
Tobias <tobias.aditya@gmail.com>
2023-01-13 21:43:01 +0700

    add create todo

--- 3-todo-list/main.go
+++ 3-todo-list/main.go
@@ -20,6 +20,8 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/todos", GetTodos).Methods(http.MethodGet)
+	r.HandleFunc("/todos", CreateTodo).Methods(http.MethodPost)
+
 	log.Println("Listening on " + baseURL)
 	http.ListenAndServe(baseURL, r)
 }
@@ -28,4 +30,14 @@ func GetTodos(w http.ResponseWriter, r *http.Request) {
 	data, _ := json.Marshal(Todos)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(data)
+}
+
+func CreateTodo(w http.ResponseWriter, r *http.Request) {
+	var t Todo
+	decoded := json.NewDecoder(r.Body)
+	decoded.Decode(&t)
+
+	//Append ke todos
+	Todos = append(Todos, &t)
+	w.Write([]byte("success add todo"))
 }
