ad71ef43dd6f7996e15ac982ab8ee852744f64f7
Tobias <tobias.aditya@gmail.com>
2023-01-13 21:54:46 +0700

    add update todo

--- 3-todo-list/main.go
+++ 3-todo-list/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -21,6 +23,7 @@ func main() {
 
 	r.HandleFunc("/todos", GetTodos).Methods(http.MethodGet)
 	r.HandleFunc("/todos", CreateTodo).Methods(http.MethodPost)
+	r.HandleFunc("/todos/{idx}", UpdateTodo).Methods(http.MethodPut)
 
 	log.Println("Listening on " + baseURL)
 	http.ListenAndServe(baseURL, r)
@@ -40,4 +43,24 @@ func CreateTodo(w http.ResponseWriter, r *http.Request) {
 	//Append ke todos
 	Todos = append(Todos, &t)
 	w.Write([]byte("success add todo"))
+}
+
+func UpdateTodo(w http.ResponseWriter, r *http.Request) {
+
+	//Get Path param
+	vars := mux.Vars(r)
+	idx := vars["idx"]
+	fmt.Println("index =" + idx)
+
+	//Read Body
+	var t Todo
+	decoded := json.NewDecoder(r.Body)
+	decoded.Decode(&t)
+
+	i, _ := strconv.Atoi(idx)
+
+	Todos[i] = &t
+
+	w.Write([]byte("success update"))
+
 }
