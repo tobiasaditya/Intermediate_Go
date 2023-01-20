546ac5fd25771f6f25be881f4a818d05c97f3b3a
Tobias <tobias.aditya@gmail.com>
2023-01-20 21:55:43 +0700

    add set session

---
+++ 5-session/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo"
+)
+
+const SESSION_ID = "test-session-id"
+
+func newCookieStore() *sessions.CookieStore {
+	authKey := []byte("my-auth-key-very-secret")
+	encryptionKey := []byte("my-encryption-key-very-secret123")
+
+	store := sessions.NewCookieStore(authKey, encryptionKey)
+	store.Options.Path = "/"
+	store.Options.MaxAge = 86400 * 7
+	store.Options.HttpOnly = true
+
+	return store
+}
+
+func main() {
+	r := echo.New()
+	store := newCookieStore()
+	r.GET("/set", func(ctx echo.Context) error {
+		session, err := store.Get(ctx.Request(), SESSION_ID)
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, err.Error())
+		}
+		session.Values["message1"] = "hello"
+		session.Values["message2"] = "world"
+		err = session.Save(ctx.Request(), ctx.Response())
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, err.Error())
+		}
+		return ctx.String(http.StatusOK, "session set")
+	})
+
+	r.Start("localhost:8080")
+}
