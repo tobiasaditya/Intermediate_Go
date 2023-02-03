913ad4637680b1ddb370e5e43ea78883e59c2336
Tobias <tobias.aditya@gmail.com>
2023-02-03 21:44:50 +0700

    change url db host to match service name

--- 9-session-login/config/config.go
+++ 9-session-login/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	DB_URI         = "postgres://postgresuser:postgrespassword@127.0.0.1:5432/postgres?sslmode=disable"
+	DB_URI         = "postgres://postgresuser:postgrespassword@postgres-db:5432/postgres?sslmode=disable"
 	AUTH_KEY       = "my-auth-key-very-secret"
 	ENCRYPTION_KEY = "my-encryption-key-very-secret123"
 	SESSION_ID     = "test-session-id"
