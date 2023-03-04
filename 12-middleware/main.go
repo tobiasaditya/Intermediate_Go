86fea56842f1ed3b27785a4a777f43e71e0683c8
Tobias <tobias.aditya@gmail.com>
2023-03-04 10:55:02 +0700

    implement echo middleware logging and init func make entry log logrus

--- 12-middleware/main.go
+++ 12-middleware/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -12,6 +15,11 @@ func main() {
 
 	e.Use(middlewareOne)
 	e.Use(middlewareTwo)
+	e.Use(echo.WrapMiddleware(middlewareNonEcho))
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}",
+	}))
 
 	e.GET("/index", func(ctx echo.Context) error {
 		fmt.Println("Masuk /index")
@@ -34,4 +42,26 @@ func middlewareTwo(next echo.HandlerFunc) echo.HandlerFunc {
 		fmt.Println("Masuk middleware two")
 		return next(ctx)
 	}
+}
+
+func middlewareNonEcho(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Masuk middleware non echo")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func makeLogEntry(c echo.Context) *logrus.Entry {
+	if c == nil {
+		return logrus.WithFields(logrus.Fields{
+			"at": time.Now().Format("2006-01-02 15:04:00"),
+		})
+	}
+
+	return logrus.WithFields(logrus.Fields{
+		"at":     time.Now().Format("2006-01-02 15:04:00"),
+		"method": c.Request().Method,
+		"uri":    c.Request().URL.String(),
+		"ip":     c.Request().RemoteAddr,
+	})
 }
